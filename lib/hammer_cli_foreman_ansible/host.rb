6f1d1126c776847969dc3d502333d3daab4afcda
Oleh Fedorenko <ofedoren@redhat.com>
2019-03-26 14:55:45 +0100

    Fixes #25446, #25647, #22832, #22831 - Blank entry, filtering (#2)

--- lib/hammer_cli_foreman_ansible/host.rb
+++ lib/hammer_cli_foreman_ansible/host.rb
@@ -1,6 +1,41 @@
+# frozen_string_literal: true
+
 module HammerCLIForemanAnsible
-  class HostAnsibleRolesCommand < BaseAnsibleRolesCommand
+  class Host < HammerCLIForeman::Command
     resource :hosts
-    build_options
+
+    class AnsibleRolesCommand < HammerCLIForeman::Command
+      command_name 'ansible-roles'
+      desc _('Manage Ansible roles on a host')
+
+      class ListCommand < HammerCLIForeman::ListCommand
+        action :ansible_roles
+
+        output HammerCLIForemanAnsible::AnsibleRolesCommand::ListCommand.output_definition
+
+        build_options
+      end
+
+      class PlayCommand < HammerCLIForeman::Command
+        command_name 'play'
+        action :play_roles
+
+        success_message _('Ansible roles are being played. Job ID: %{id}')
+        failure_message _('Could not play roles on a host')
+
+        build_options
+      end
+
+      class AssignRolesCommand < HammerCLIForeman::Command
+        command_name 'assign'
+        action :assign_ansible_roles
+
+        success_message _('Ansible roles were assigned to the host')
+        failure_message _('Could not assign roles to the host')
+
+        build_options
+      end
+      autoload_subcommands
+    end
   end
 end
--- lib/hammer_cli_foreman_ansible/version.rb
+++ lib/hammer_cli_foreman_ansible/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module HammerCLIForemanAnsible
   def self.version
     @version ||= Gem::Version.new '0.1.2'
