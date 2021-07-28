0086a87d9708d2d5d67bfbc09151e5cab38f6d99
Ondrej Prazak <oprazak@redhat.com>
2021-07-28 14:58:05 +0200

    Fixes #33165 - Fix undefined resolver method with katello

--- lib/hammer_cli_foreman_ansible/command_extensions/resolver.rb
+++ lib/hammer_cli_foreman_ansible/command_extensions/resolver.rb
@@ -1,7 +1,7 @@
 module HammerCLIForemanAnsible
   module ResolverExtension
     def create_ansible_roles_search_options(options, mode = nil)
-      if defined? HammerCLIKatello::IdResolver
+      if self.class.to_s == 'HammerCLIKatello::IdResolver'
         create_search_options_without_katello_api(options, api.resource(:ansible_roles), mode)
       else
         create_search_options(options, api.resource(:ansible_roles), mode)
